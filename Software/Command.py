6ddb971cb32877ea5c3565840ca3d54ed00de6a4
hotwinter <xiaoba2008@gmail.com>
2014-06-19 00:31:39 +0800

    Software Change
    Added argparse, checked length, missing bit and extra 0 at the start is
    still not fixed

--- Software/Command.py
+++ Software/Command.py
@@ -16,7 +16,8 @@ SRCLR_SEL='0'
 NCOUT21_25='00000'
 LSB_SIX_BITS=GLOBAL_READOUT_ENABLE+SRDO_LOAD+NCOUT2+COUNT_HITS_NOT+COUNT_ENABLE+COUNT_CLEAR_NOT
 CLOCK_UNIT_DURATION = 4
-LEN_CONFIG = 176
+LEN_CONFIG = 720
+LEN_COLUMN = 260
 
 #######################################################################################################################
 # Utility Functions
@@ -38,6 +39,9 @@ def binary_string(string,bits):
     if(len(b_string) < bits):
         b_string = '0'*(bits-len(b_string))+b_string
     return b_string
+def repeat_each(string,n):
+    """repeat each bit n times"""
+    return "".join([s*n for s in string])
 #######################################################################################################################
 # Basic Command construction
 # Commands are reversed because we are using a shift register
@@ -48,61 +52,64 @@ def get_control_pattern(col,hit_or = '0', hit='0',inject='0',lden='0',S0='0',S1=
     column_address = binary_string(col, 6)
     return (LSB_SIX_BITS + S0 + S1 + config_mode + hit_or + hit + inject + TDAC + lden + SRCLR_SEL + hitld_in + NCOUT21_25 + column_address)[::-1]
 
-def get_dac_pattern(vth=150, DisVbn=49, VbpThStep=100, PrmpVbp=142, PrmpVbnFol=35, PrmpVbf=11): # fol35 vbp142 vbf11
+def get_dac_pattern(vth=150, DisVbn=49, VbpThStep=100, PrmpVbp=142, PrmpVbnFol=35, PrmpVbf=11, empty = False): # fol35 vbp142 vbf11
     """This is the first 144bit(Counting from LSB) out of 176 bits in the configuration register,
-    It is only called once during the set_config command, and never changed afterwards"""
+    It is only called once during the set_config command, and never changed afterwards
+    If empty is true, generate 144 bit 0s,
+    8 zeros at the front is to pull down the voltage"""
     default=binary_string(129,8)# just for padding, no special meaning
-    return ("00000000"+default*3 + binary_string(DisVbn,8) + default*6 + binary_string(VbpThStep,8) + binary_string(PrmpVbp,8) + binary_string(PrmpVbnFol,8) + binary_string(vth,8) + binary_string(PrmpVbf,8) + default+"00000000")[::-1]
-
-def pixel_command(row,num,is_hit_or=True,enable=True):
-    """This method generates a pixel command, which start at row and enables num pixels after it.
-    Pixel index Starts with row 0
-    hit_or True, enable True: enable command for hit_or
-    hit_or True, enable False: disable command for hit_or
-    hit_or False, enable True: enable command for hit_inject
-    hit_or False, enable False, disable command for hit_inject"""
-    if (is_hit_or and enable) or (not is_hit_or and not enable):
-        return ('1'*(row)+'0'*num+ '1'*(MAXROWS-row-num))[::-1]
+    if empty:
+        return "0"*144
     else:
-        return ('0'*(row)+'1'*num+ '0'*(MAXROWS-row-num))[::-1]
+        return ("00000000"+default*3 + binary_string(DisVbn,8) + default*6 + binary_string(VbpThStep,8) + binary_string(PrmpVbp,8) + binary_string(PrmpVbnFol,8) + binary_string(vth,8) + binary_string(PrmpVbf,8) + default+"00000000")[::-1]
+
 #######################################################################################################################
 #CommandDict Generation and combine
-def _gen_single_command(pattern, load_dacs=False, load_control=True, config=True):
-    """Generate a command dictionary
-     if config True, generate a config command, else generate a column command
+def gen_config_command(pattern, load_dacs=False, load_control=True):
+    """Generate a config command dictionary
     a pulse on load_dacs loads the first 144 bits in the config shift register
     a pulse on load_control loads the last 32 bits in the config shift register
     By default, this does not load the first 144 bits"""
     load_dacs = '1' if load_dacs else '0'
     load_control = '1' if load_control else '0'
-    pull_down = '0'
+
+    SregData=repeat_each(pattern,CLOCK_UNIT_DURATION)
+    SregPat=SregData+'0'*(LEN_CONFIG-len(SregData)-1)
+    SregPat='0'+SregPat
+
+    ClkData=generate_clock(len(pattern),CLOCK_UNIT_DURATION/2)
+    ClkPat=ClkData+'0'*(LEN_CONFIG-len(ClkData))
+
+    # Make sure the load (ctrl and dac) are set correctly 
+    LDZeroLengthBefore=len(ClkData) + 2
     LDPatLength=4
-
-    if config:
-        length = LEN_CONFIG
-    else:
-        length = MAXROWS
-
-    SregData=''.join([bit*CLOCK_UNIT_DURATION for bit in pattern])
-    SregPat='0'+(length*CLOCK_UNIT_DURATION-len(SregData))*'0'+SregData+(1+LDPatLength)*'0'+pull_down
-    ClkData=generate_clock(length,CLOCK_UNIT_DURATION/2)
-    ClkPat = ClkData+(2+LDPatLength)*'0'+pull_down
-
-    # Make sure the load (ctrl and dac) are set correctly
-    LDZeroLengthBefore= len(ClkData)+2
-
-    LDPat='0'*LDZeroLengthBefore + load_control*LDPatLength+'0'
-    LD_dacsPat='0'*LDZeroLengthBefore + load_dacs*LDPatLength+'0'
+    LDPat='0'*LDZeroLengthBefore + load_control*LDPatLength + '0'*(LEN_CONFIG-LDZeroLengthBefore-LDPatLength)
+    LD_dacsPat='0'*LDZeroLengthBefore + load_dacs*LDPatLength + '0'*(LEN_CONFIG-LDZeroLengthBefore-LDPatLength)
     emptyPat='0'*len(LDPat)
 
-    if config:
-        commands_dict = {'Stbld':LDPat,'Dacld':LD_dacsPat,'GCfgCK':ClkPat,'SRIN_ALL':SregPat,'SRCK_G':emptyPat,'NU':emptyPat}
-    else:
-        commands_dict = {'Stbld':emptyPat,'Dacld':emptyPat,'GCfgCK':emptyPat,'SRIN_ALL':SregPat,'SRCK_G':ClkPat,'NU':emptyPat}
+    commands = {'Stbld':LDPat,'Dacld':LD_dacsPat,'GCfgCK':ClkPat,'SRIN_ALL':SregPat,'SRCK_G':emptyPat,'NU':emptyPat}
 
-    #print([len(i) for i in commands_dict.values()])
-    return commands_dict
+    return commands
 
+def gen_column_command(pattern):
+    """Generate a command which will be used to program a column."""
+    if len(pattern) != MAXROWS:
+        print "Invalid pattern length for program_column (should be %i). Appending zeroes." % MAXROWS
+        pattern += '0'*(MAXROWS - len(pattern))
+
+    colSreg=pattern[::-1]
+    SregData=repeat_each(colSreg,CLOCK_UNIT_DURATION)
+    SregPat=SregData+'0'*(LEN_COLUMN-len(SregData)-1)
+    SregPat='0'+SregPat
+    ClkData=generate_clock(len(colSreg),CLOCK_UNIT_DURATION/2)
+    ClkPat= ClkData+'0'*(LEN_COLUMN-len(ClkData))
+    emptyPat='0'*len(ClkPat)
+
+    commands = {'Stbld':emptyPat,'Dacld':emptyPat,'GCfgCK':emptyPat,'SRIN_ALL':SregPat,'SRCK_G':ClkPat,'NU':emptyPat}
+
+    return commands
+    
+    
 def command_Dict_combine(*args):
     """Combining multiple command dictionaries, this used in more complex commands associated with pixel commands"""
     combined_dict=defaultdict(str)
@@ -113,50 +120,24 @@ def command_Dict_combine(*args):
 ########################################################################################################################
 #Complex command dictionary generation
 def set_config(vth=150,PrmpVbp=142,PrmpVbf=11,config_mode="00"):
-    return _gen_single_command(get_dac_pattern(vth=vth,PrmpVbp=PrmpVbp,PrmpVbf=PrmpVbf)+get_control_pattern(63,config_mode=config_mode),load_dacs=True)
+    return gen_config_command(get_dac_pattern(vth=vth,PrmpVbp=PrmpVbp,PrmpVbf=PrmpVbf)+get_control_pattern(63,config_mode=config_mode),load_dacs=True)
 
 def point_to_column(col,config_mode):
     """Used only in pixel commands to point the shift_register to correct column"""
-    return _gen_single_command(get_control_pattern(col,config_mode=config_mode))
-
-def load_pixel_reg(row,is_hit_or,num,enable):
-    return _gen_single_command(pixel_command(row=row,num=num,is_hit_or=is_hit_or,enable=enable),config=False)
+    return gen_config_command(get_dac_pattern(empty=True)+get_control_pattern(col,config_mode=config_mode))
 
 def load_ldbus(col,hit_or,hit,inject):
-    return _gen_single_command(get_control_pattern(col,hit_or=str(hit_or),hit=str(hit),inject=str(inject),lden="1"))
+    return gen_config_command(get_dac_pattern(empty=True)+get_control_pattern(col,hit_or=str(hit_or),hit=str(hit),inject=str(inject),lden="1"))
 
-def hitor_hit_inject(col=0,row=0,is_hit_or=True,single_pixel=False,single_column=False,all=False,enable=True):
-    """hit_or, hit, inject manipulation"""
-    if(is_hit_or):
-        hit_or,hit,inject=1,0,0
-    else:
-        hit_or,hit,inject=0,1,1
-    if(all):
-        config_mode="11"
-    elif(single_column or single_pixel):
-        config_mode = "00"
-        if(single_column):
-            num=64
-        else:
-            num=1
-    else:
-        raise Exception
-    return command_Dict_combine(point_to_column(col,config_mode),load_pixel_reg(row,is_hit_or,num,enable),load_ldbus(col,hit_or,hit,inject),point_to_column(col,config_mode))
-
-def Gcfg_TEST(index):
+def Gcfg_Test(index):
     """A bitpattern of a '1' only at the associated index, this is only used to test the shift register, check if everything is working
     Clock into GcfgCK, Data into SRIN_ALL, Readout GcfgCK, DO NOT LOAD PATTERN
     Index Starts at 0
     """
-    return _gen_single_command('0'*index+'1'+'0'*(175-index),load_control=False)
+    return gen_config_command('0'*index+'1'+'0'*(175-index),load_control=False)
 
-def Pixel_Array_Test_1(col,row,num):
-    """A bitpattern of a '1' only at the associated index, this is only used to test the pixel register, check if everything is working
-    Step 1: Clock into GcfgCk to send configuration,into SRIN_ALL to point to the column we want to test
-    Step 2: Select which pixel we want to load to. Clock SR_CK for this
-    Step 3: Set lden to True to load it into each 8 bit register of the individual pixels, Clock GcfgCK for this
-    Step 4: Repeat Step 2 and 3 again inorder to shift out what we have sent.(We should See data at SR_OUT)"""
-    return command_Dict_combine(point_to_column(col,"00"),load_pixel_reg(row,False,num,True),load_ldbus(col,1,0,0))
+def Column_Array_Test(row,num):
+    """A bitpattern of a '1' only at the associated index, this is only used to test the column register, check if everything is working"""
 
-def Pixel_Array_Test_2(col,row,num):
-    return command_Dict_combine(load_pixel_reg(row,False,num,True),load_ldbus(col,1,0,0),point_to_column(col,'00'))
+    col_pat='0'*row+'1'*num+'0'*(MAXROWS-row-num)
+    return gen_column_command(col_pat)
--- Software/FPGAgen.py
+++ Software/FPGAgen.py
@@ -1,6 +1,7 @@
 __author__ = 'Maximilian Golub','Bo Wang'
 import serial
 import Command
+import argparse
 #######################################################################################################################
 #Settings for serial communication, predefined FPGA commands and pin mapping
 BAUD = 9600
@@ -61,17 +62,15 @@ def convertFPGAHits(data):
     return final
 ########################################################################################################################
 #Single Command generation methods
-def commandRead(commandDict):
+def commandRead(commandDict,sendFile):
     """Takes a dictionary of command constructed in Command.py and return a data bus list"""
     stringList = []
     for i in range(8):
         stringList.append(commandDict[pinDict[i]])
-    shiftData = open('shiftData_before.txt', 'a')
     for s in pinDict.values():
         if (s != "NU"):
-            shiftData.write(s+"\n")
-            shiftData.write(commandDict[s]+"\n")
-    shiftData.close()
+            sendFile.write(s+"\n")
+            sendFile.write(commandDict[s]+"\n")
     return convertToByte(stringList)
 
 def convertToRaw(string):
@@ -95,34 +94,33 @@ def FPGA_write(port, commandString, RX_ON = True):
         print("Serial port failure")
         raise Exception
 
-def readData(port,lenData):
+def readData(port,lenData,readFile,if_read):
     """Reads data from the serial port to a file called shiftData.txt."""
-    shiftData = open('shiftData.txt', 'a')
     FPGA_write(port,TRANSMIT,False)
-    data = port.read(lenData-1)
-    final = convertFPGAHits(data)
-    shiftData.write(final+"\n")
-    shiftData.close()
+    data = port.read(lenData)
+    if if_read:
+        final = convertFPGAHits(data)
+        readFile.write(final+"\n")
 
-def commonSetup(port,commandDict):
+def commonSetup(port,commandDict,sendFile):
     """Common setup between auto and manual methods"""
     port.close()
     port.open()
-    byteCMDString = commandRead(commandDict)
+    byteCMDString = commandRead(commandDict,sendFile)
     len_Data = FPGA_write(port,byteCMDString)
     return len_Data
 
-def manual(port,commandDict):
+def manual(port,commandDict,sendFile,readFile,if_read=True):
     """The manual method of controlling T3MAPS.
     The user must manually verify each step"""
-    len_Data=commonSetup(port,commandDict)
+    len_Data=commonSetup(port,commandDict,sendFile)
     Winput = raw_input("Write data to T3MAPS? (y/n): ")
 
     if (Winput.lower() == "y"):
         FPGA_write(port,WRITE,False)
         Winput2 = raw_input("Read data from FGPA fifo? (y/n): ")
         if (Winput2.lower() == "y"):
-            readData(port,len_Data)
+            readData(port,len_Data,readFile,if_read)
             port.close()
         else:
             print("Read aborted")
@@ -131,11 +129,11 @@ def manual(port,commandDict):
         print("Write aborted")
         port.close()
 
-def auto(port,commandDict):
+def auto(port,commandDict,sendFile,readFile,if_read=True):
     """The automatic method to write a stream to control T3MAPS.
 Will not lose data due to built in buffer in computer"""
-    len_Data=commonSetup(port,commandDict)
-    readData(port, len_Data)
+    len_Data=commonSetup(port,commandDict,sendFile)
+    readData(port, len_Data,readFile,if_read)
 
 
 def analog_Test(port,type):
@@ -146,26 +144,84 @@ def analog_Test(port,type):
     auto(port,Command.hitor_hit_inject(is_hit_or=True,all=True,enable=True))
     auto(port,Command.hitor_hit_inject(is_hit_or=False,all=True,enable=False))
 
-def Test_Pattern_Gcfg(port,num):
+def Test_Pattern_Gcfg(args):
     """This is to run the system with test patterns for Gcfg Register, it will
     output a 1 at an index starting from 0, this way we can verify if that specific
     bit is at the right place when we read it back"""
-    if(num>176):
+    sendFile=open(args.sendFile,"wb")
+    readFile=open(args.readFile,"wb")
+    if(args.num>176):
         raise Exception
-    for i in range(0,num):
-        auto(port,Command.command_Dict_combine(Command.Gcfg_TEST(i),Command.Gcfg_TEST(i)))
+    for i in range(0,args.num):
+        sendFile.write(str(i)+": \n")
+        auto(args.port,Command.Gcfg_Test(i),sendFile,readFile,if_read=False)
+        readFile.write(str(i)+": \n")
+        auto(args.port,Command.Gcfg_Test(i),sendFile,readFile)
+        readFile.write("\n")
+        sendFile.write("\n")
+    sendFile.close()
+    readFile.close()
+
+
+def Test_Pattern_Column(args):
+    sendFile=open(args.sendFile,"wb")
+    readFile=open(args.readFile,"wb")
+    if(args.num>64):
+        raise Exception
+    for i in range(0,args.num):
+        sendFile.write(str(i)+": \n")
+        auto(args.port,Command.command_Dict_combine(Command.point_to_column(args.col,"00"),Command.Column_Array_Test(i,1)),sendFile,readFile,if_read=False)
+        readFile.write(str(i)+": \n")
+        auto(args.port,Command.Column_Array_Test(i,1),sendFile,readFile)
+        readFile.write("\n")
+        sendFile.write("\n")
+    sendFile.close()
+    readFile.close()
+
+def main_setup(args):
+    print " Orient the chip, so that VDDA VDDD is in the correct position"
+    print "  The power supply on the right should be connected to 1.5V (J30,J29)"
+    print "  Another choice is to connect the left pin of J57 and J56 to 1.5V and the right pin to GND"
+    print "  Make Sure SelAltBus is connected to the inner pin(Inner pin is connected to VDDD)"
+    print "  The substrate supply on the left should not be floating, connect it to ground (J33)\n"
+    print "  Check PinDict in code, modify it to suit your own need\n"
+    print "  Connect the specified command number pin on adapter card to the specified pin on T3MAPS"
+
+def set_config(args):
+    sendFile=open(args.sendFile,"wb")
+    readFile=open(args.readFile,"wb")
+    auto(args.port,Command.set_config(),sendFile,readFile)
 
 #Call the main method upon execution.
 if __name__ == "__main__":
     port = serial.Serial(port=COMPORT,baudrate=BAUD, bytesize=8,stopbits=2, timeout=TIMEOUT)
-    #auto(port,Command.set_config())
-    #auto(port,Command.set_config())
-    #Test_Pattern_Gcfg(port,1)
-    #col = raw_input("Which Column to point to?")
-    col = 7
-    row = 0
-    num = 64
-    auto(port,Command.Pixel_Array_Test_1(col,row,num))
-    auto(port,Command.Pixel_Array_Test_2(col,row,num))
-    #auto(port,Command.point_to_column(1,"00"))
-    #auto()
+
+    parser = argparse.ArgumentParser(description="Present a few options from FPGAgen.py to be used from the command line for testing..")
+    parser.set_defaults(port=port)
+    subparsers = parser.add_subparsers(title = 'Functions')
+
+    setup = subparsers.add_parser('setup', help='Print instructions for the physical setup of the chip.')
+    setup.set_defaults(func=main_setup)
+
+    config_test = subparsers.add_parser('config_test', help='Test the outer configuration register, Input at SRIN_ALL, output at GcfgDout')
+    config_test.set_defaults(func=Test_Pattern_Gcfg)
+    config_test.add_argument('--num', dest='num', type=int, default=1, help='Number of test you want to perform, each '
+     'test is only one 1 at the specific index, and 0s elsewhere. So if you set num=2, then you will be testing two times, first time with 1 at index 0, second time with 1 at index 1')
+    config_test.add_argument('--sendFile',dest='sendFile',type=str, default='shiftData_before.txt', help='file to store sent data')
+    config_test.add_argument('--readFile',dest='readFile',type=str, default='shiftData.txt',help='file to store data read back')
+
+    column_test = subparsers.add_parser("column_test", help='Test the inner column shift register, Input at SRIN_ALL, output at SR_OUT')
+    column_test.set_defaults(func=Test_Pattern_Column)
+    column_test.add_argument('--col',dest='col',type=int,default=0,help='Column you wish to test')
+    column_test.add_argument('--num', dest='num', type=int, default=1, help='Number of test you want to perform, each '
+     'test is only one 1 at the specific index, and 0s elsewhere. So if you set num=2, then you will be testing two times, first time with 1 at index 0, second time with 1 at index 1')
+    column_test.add_argument('--sendFile',dest='sendFile',type=str, default='shiftData_before.txt', help='file to store sent data')
+    column_test.add_argument('--readFile',dest='readFile',type=str, default='shiftData.txt',help='file to store data read back')
+
+    config = subparsers.add_parser("set_config", help='Set configuration for T3MAPS, 20mA current expected')
+    config.set_defaults(func=set_config)
+    config.add_argument('--sendFile',dest='sendFile',type=str, default='shiftData_before.txt', help='file to store sent data')
+    config.add_argument('--readFile',dest='readFile',type=str, default='shiftData.txt',help='file to store data read back')
+
+    args = parser.parse_args()
+    args.func(args)
